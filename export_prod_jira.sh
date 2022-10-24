7f896a4231a35dc9f4008f72b2860b0c2de470e4
Pete Santos <50371603+petesantos@users.noreply.github.com>
2022-10-24 11:12:47 -0400

    Update docker file and shell script

--- export_prod_jira.sh
+++ export_prod_jira.sh
@@ -2,15 +2,15 @@
 
 JIRA_ISSUE=${1}
 VAULT_TOKEN=${2:-$(cat ~/.vault-token)}
-JIRA_TOKEN=$(docker run -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault read --format=json secret/dsde/firecloud/dev/common/jira.conf | jq -r .data.jira_token)
+JIRA_TOKEN=$(vault read --format=json secret/dsde/firecloud/dev/common/jira.conf | jq -r .data.jira_token)
 JIRA_USER=jenkins-jira@broad.mit.edu
 
 
-docker run -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault read --format=json secret/dsde/terra/slas/jenkins-automation-service-account.json | jq -r .data > svcacct.json
+vault read --format=json secret/dsde/terra/slas/jenkins-automation-service-account.json | jq -r .data > svcacct.json
 
 # Run locally (default)
 python parse_jira_issues.py --apiUser ${JIRA_USER} --apiToken ${JIRA_TOKEN} --issue ${JIRA_ISSUE} --bigquerySvcAcct svcacct.json
 
 # Run in docker (for automation)
-#docker build -t sla-import .
-#docker run -v svcacct.json:/app sla-import python parse_jira_issues.py --apiUser ${JIRA_USER} --apiToken ${JIRA_TOKEN} --issue ${JIRA_ISSUE} --bigquerySvcAcct svcacct.json
+# docker build -t sla-import .
+# docker run -v "$(pwd)"/svcacct.json:/app/svcacct.json sla-import python parse_jira_issues.py --apiUser ${JIRA_USER} --apiToken ${JIRA_TOKEN} --issue ${JIRA_ISSUE} --bigquerySvcAcct svcacct.json
